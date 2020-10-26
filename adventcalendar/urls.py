a72ac1100e65d8bba021ceae7e9f9a64b82f00ce
gvarab <marta@2e2a.de>
2020-10-26 17:59:02 +0100

    CreateViews for calendar and door

--- adventcalendar/urls.py
+++ adventcalendar/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from adventcalendar.views import CalendarList
-from adventcalendar.views import CalendarDetail
-from adventcalendar.views import DoorDetail
+from adventcalendar.views import CalendarList, CalendarDetail, DoorDetail, NewCalendar, EditCalendar, NewDoor
 
 urlpatterns = [
     path('', CalendarList.as_view(), name='calendar_list'),
     path('<int:pk>/', CalendarDetail.as_view(), name='calendar_detail'),
     path('<int:cal>/<int:pk>/', DoorDetail.as_view(), name='door_detail'),
+    path('new_calendar/', NewCalendar.as_view(), name='calendar_new'),
+    path('<int:pk>/edit/', EditCalendar.as_view(), name='calendar_edit'),
+    path('<int:cal>/new_door/', NewDoor.as_view(), name='door_new'),
 ]
--- adventcalendar/views.py
+++ adventcalendar/views.py
@@ -1,19 +1,49 @@
-from django.views.generic import ListView
-from django.views.generic import DetailView
-from adventcalendar.models import Calendar
-from adventcalendar.models import Door
+from django.urls import reverse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from .models import Calendar, Door
 
 
 class CalendarList(ListView):
-
     model = Calendar
 
 
 class CalendarDetail(DetailView):
-
     model = Calendar
 
 
 class DoorDetail(DetailView):
-
     model = Door
+
+
+class NewCalendar(CreateView):
+    model = Calendar
+    fields = ['name', ]
+
+    def get_success_url(self):
+        return reverse('calendar_list')
+
+
+class EditCalendar(UpdateView):
+    model = Calendar
+    fields = ['name', ]
+
+    def get_success_url(self):
+        return reverse('calendar_list')
+
+
+class NewDoor(CreateView):
+    model = Door
+    fields = ['number', 'content', ]
+    calendar = None
+
+    def dispatch(self, request, *args, **kwargs):
+        calendar_pk = kwargs.get('cal')
+        self.calendar = Calendar.objects.get(pk=calendar_pk)
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_success_url(self):
+        return reverse('calendar_detail', args=[self.calendar.pk])
+
+    def form_valid(self, form):
+        form.instance.calendar = self.calendar
+        return super().form_valid(form)
