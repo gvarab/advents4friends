989d69f59d577f705ef1f8e8c0f9aec68d1242bb
gvarab <marta@2e2a.de>
2020-10-30 18:59:34 +0100

    include bootstrap

--- advents4friends/settings.py
+++ advents4friends/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+CONFIG_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(CONFIG_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(CONFIG_DIR, 'static'),
+]
