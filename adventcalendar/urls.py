fcf4d7f16f16f2f97c8a5dbf4ef63f396ced5a59
gvarab <marta@2e2a.de>
2020-11-05 17:12:48 +0100

    different style for page and calendar headers

--- adventcalendar/urls.py
+++ adventcalendar/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     path('', CalendarList.as_view(), name='calendar_list'),
     path('<int:pk>/', CalendarDetail.as_view(), name='calendar_detail'),
     path('<int:cal>/<int:pk>/', DoorDetail.as_view(), name='door_detail'),
+    path('<int:cal>/<int:pk>/edit/', EditDoor.as_view(), name='door_edit'),
     path('new_calendar/', NewCalendar.as_view(), name='calendar_new'),
     path('<int:pk>/edit/', EditCalendar.as_view(), name='calendar_edit'),
     path('<int:pk>/delete/', DeleteCalendar.as_view(), name='calendar_delete'),
-    path('<int:cal>/<int:pk>/edit/', EditDoor.as_view(), name='door_edit'),
+
 ]
