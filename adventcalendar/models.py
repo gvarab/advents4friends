376eac6b3536e9bbe5c29ec536c786f2317635ef
gvarab <marta@2e2a.de>
2020-11-07 12:37:31 +0100

    change urls

--- adventcalendar/models.py
+++ adventcalendar/models.py
@@ -23,7 +23,7 @@ class Calendar(models.Model):
 
     def initialize_doors(self):
         for i in range(0, self.number_of_doors):
-            Door.objects.create(number=i+1, content="", opening_date=self.start_date + timedelta(days=i),
+            Door.objects.create(number=i + 1, content="", opening_date=self.start_date + timedelta(days=i),
                                 calendar=self)
 
 
@@ -32,3 +32,10 @@ class Door(models.Model):
     content = models.TextField(max_length=1000)
     opening_date = models.DateField(default=timezone.now)
     calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE, null=True)
+    open = models.BooleanField(default=False)
+
+    def short_content(self):
+        short_content = self.content
+        if len(self.content) > 10:
+            short_content = short_content[0:10] + '...'
+        return short_content
