9caf36aba917d6e375f71500e9642407c856053c
gvarab <marta@2e2a.de>
2020-11-18 16:55:44 +0100

    add registration

--- adventcalendar/urls.py
+++ adventcalendar/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 from adventcalendar.views import CalendarList, CalendarDetail, DoorDetail, NewCalendar,\
     EditCalendar, EditDoor, DeleteCalendar
 
@@ -10,4 +10,6 @@ urlpatterns = [
     path('calendar/<slug:slug>/delete/', DeleteCalendar.as_view(), name='calendar_delete'),
     path('door/<slug:slug>/', DoorDetail.as_view(), name='door_detail'),
     path('door/<slug:slug>/edit/', EditDoor.as_view(), name='door_edit'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
